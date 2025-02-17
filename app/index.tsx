7ea038110969c3252918c68cd9601a775957494c
Edgar Vergara <edgarvm13@hotmail.com>
2025-02-17 11:23:33 +0100

    ✨ Add various assets and update Tailwind CSS configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,17 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="font-pblack">Aora!</Text>
 
       <StatusBar style="auto" />
 
-      <Link href="/profile" className="text-4xl">
+      <Link href="/profile" className="text-blue-600">
         Go to Profile Page
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
